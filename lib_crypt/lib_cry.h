0534183a373237c4cf447e367eb96ce5e2c690ce
ZlojKOTA <zlojkota@gmail.com>
2015-04-30 16:34:35 +0600

    Writed 40% functionals of decrypt

--- lib_crypt/lib_cry.h
+++ lib_crypt/lib_cry.h
@@ -2,13 +2,21 @@
 #define LIB_CRY_H_INCLUDED
 
 //_buf   - loop bufer in 64 symbols.
-//_head  - last pushed symbol
-//_count - last pulled symbol
+//_buf_head  - last pushed buffer symbol
+//_buf_count - last pulled buffer symbol
+//_count - counter crypt/decrypt symbol
 //If _head=_count - NOP
+//incom_crypt    - crypted   input
+//incom_decrypt  - decrypted output
+//outcom_decrypt - decrypted input
+//outcom_crypt   - crypted   output
 
+const unsigned char  header_const[]=[0xFE,0xA0,'S','k','O','t','A'];
 unsigned char incom_crypt_buf   [64];
 unsigned char incom_crypt_buf_count ;
-unsigned char incom_crypt_buf_head ;
+unsigned char incom_crypt_buf_head  ;
+unsigned char incom_crypt_count     ;
+unsigned char incom_crypt_length    ;
 
 unsigned char incom_decrypt_buf [64];
 unsigned char incom_decrypt_buf_count ;
